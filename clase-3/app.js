efe9b30b33ac7f991a5df1dbaa3de201ee1c2c86
Miguel <miguelanto0512@gmail.com>
2025-07-21 10:42:28 -0500

    Se agrega la clase 3 terminada

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,11 +1,39 @@
 const express = require('express')
 const crypto = require('node:crypto')
+const cors = require('cors')
 const movies = require('./movies.json')
 const { validateMovie, validatePartialMovie } = require('./schemas/movie')
 
+// ? Métodos normales: GET/HEAD/POST
+// ? Métodos complejos: PUT/PATCH/DELETE/
+
+// ? CORS = Cross Origin Resource Sharing
+
+// ? CORS PRE-Flight: AL HACER UNA PETICIÓN COMPLEJA REQUIERE UNA PETICIÓN ESPECIAL LLAMADA OPTIONS
+
 const app = express()
-app.disable('x-powered-by')
 app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://Localhost:1234',
+      'http://Localhost:5500',
+      'https://movies.com',
+      'http://127.0.0.1:5500'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
+app.disable('x-powered-by')
 
 // ? Todos los recursos que sean MOVIES se identifican con la ruta /movies
 
@@ -45,6 +73,19 @@ app.post('/movies', (req, res) => {
   res.status(201).json(newMovie) // ? Actualizar la cachè del cliente
 })
 
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ error: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted successfully' })
+})
+
 app.patch('/movies/:id', (req, res) => {
   const result = validatePartialMovie(req.body)
 
@@ -68,7 +109,7 @@ app.patch('/movies/:id', (req, res) => {
   return res.json(updateMovie)
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 1234
 
 app.listen(PORT, () => {
   console.log(`server is listening on port http://localhost:${PORT}`)
